e828e00e8c07a09c532eddc42861466a499959bc
lbry-whitmerr <lbry-whitmerr@dhcp-10-142-163-69.uawifi.arizona.edu>
2023-04-28 16:49:33 -0700

    adds news cards to front-page

--- front-page.php
+++ front-page.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Template Name: Homepage 
+ * Template Name: Homepage
  */
- 
- 
+
+
  get_header();
- 
+
  $data = [];
  $args = array(
 	 'post_type' => 'grantee-project',
@@ -13,8 +13,8 @@
 	 'orderby' => 'title',
 	 'order' => 'ASC'
   );
-  
-  
+
+
   $acf_fields = ['grantee-location',
                  'grantee_organization',
                  'grantee_principal_link',
@@ -24,14 +24,14 @@
                  'grantee_bio',
                  'grantee_background_video'
                ];
- 
+
   $grantees = new WP_Query( $args );
- 
+
   foreach($grantees->posts as $key => $row) {
 	  $data[$key] = $row;
 	  $data[$key]->taxonomies = get_the_terms($row->ID,'grantee-location');
 	  $data[$key]->acf = get_fields($row->ID);
-	  
+
     // Set default values for expected acf fields
     foreach( $acf_fields as $field ) {
       if( ! isset( $data[$key]->acf[$field] ) ) {
@@ -51,10 +51,10 @@
 	 </div><!-- end .row -->
  </div><!-- end #intro -->
  <div id="tiles" class="container">
-	  <div class="row justify-content-sm-center">	  
+	  <div class="row justify-content-sm-center">
  <?php foreach( $data as $key => $row ) { if( $row->acf['grantee_background_image'] ) { ?>
-   
-   <?php if( $row->acf['grantee_background_video'] ) { ?> 
+
+   <?php if( $row->acf['grantee_background_video'] ) { ?>
   <div class="col col-12 col-sm-12 col-lg-4">
       <div class="rbn-card loading"  data-bg="<?php echo $row->acf['grantee_background_image'];?>">
         <a href="<?php echo get_the_permalink($row->ID);?>"></a>
@@ -63,19 +63,38 @@
             <source src="<?php echo $row->acf['grantee_background_video'];?>" type="video/mp4">
           </video>
       </div>
-    </div><!-- end .ratio -->   
-   <?php } else {?> 
+    </div><!-- end .ratio -->
+   <?php } else {?>
   <div class="col col-12 col-sm-12 col-lg-4">
      <div class="rbn-card loading"  data-bg="<?php echo $row->acf['grantee_background_image'];?>">
        <a href="<?php echo get_the_permalink($row->ID);?>"></a>
        <label><?php echo $row->acf['grantee_organization'];?></label>
      </div>
-   </div><!-- end .ratio -->   
+   </div><!-- end .ratio -->
     <?php } ?>
-	 
-	 <?php } } ?>		
+
+	 <?php } } ?>
 	  </div><!-- end .row -->
   </div><!-- end #tiles-->
+  <?php wp_reset_postdata();?>
+  <?php
+  /**
+   * News Items
+   */
+  $params = ['post_type' => 'news', 'posts_per_page' => '4'];
+  $query = new WP_Query($params); ?>
+  <?php if( $query->have_posts() ) : ?>
+<div class="container">
+  <div class="row d-flex align-content-stretch flex-wrap pb-6">
+  <?php while( $query->have_posts()) : $query->the_post(); ?>
+	  <div class="col-xl-6 p-4">
+		  <div class="has-ivory-background-color p-5 h-100"><?php get_template_part( 'template-parts/archive-news', 'archive-news', ['id' => get_the_ID()] );?></div>
+	  </div>
+ <?php endwhile;?>
+  </div><!-- /.row -->
+</div><! /.container -->
+ <?php endif;?>
+
  <?php
+ wp_reset_postdata();
  get_footer();
- 
