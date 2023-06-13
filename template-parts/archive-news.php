fc656f207c9ad705c64590679a0870aa5f3448ef
lbry-whitmerr <lbry-whitmerr@dhcp-10-142-150-156.uawifi.arizona.edu>
2023-06-13 13:56:24 -0700

    updates link styles on archive-event template part

--- template-parts/archive-news.php
+++ template-parts/archive-news.php
@@ -7,6 +7,6 @@ extract($args);
 <div class="archive-news-wrapper pb-4">
 	<time class="p" datetime="<?php echo $datetime ;?>"><?php echo $date ;?></time>
 	<h2><?php echo $title;?></h2>
-	<div class="pb-4"><?php echo $excerpt ;?></div>
+	<div class="pb-4"><?php echo $excerpt;?></div>
 	<a class="readmore" href="<?php echo $permalink;?>"><?php _e('Read This News Item');?></a>
 </div>
