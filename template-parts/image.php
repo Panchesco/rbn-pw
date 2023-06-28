29b793229fa45aa8d93956d8ecb6c2bb8e681e43
lbry-whitmerr <lbry-whitmerr@dhcp-10-142-177-99.uawifi.arizona.edu>
2023-06-28 11:24:41 -0700

    updates to add custom field handling of Spanish language alt tags.

--- template-parts/image.php
+++ template-parts/image.php
@@ -4,9 +4,10 @@ $img = rbn_get_attachment($image,'rbn-large');
 if( isset( $image ) && ! empty( $image ) ) { ?>
 	<figure class="p-0 m-0 w-100">
 		<div class="media">
-		<img class="img-fluid" src="<?php echo $img->url;?>" alt="<?php echo $img->alt_text;?>" />
+			<img src="<?php echo $img->url;?>" class="img-fluid" loading="lazy" alt="<?php echo $img->alt_text;?>" />
+		</div>
 		<?php if( isset( $image_caption )  && ! empty( $image_caption ) ) {?>
-	<figcaption class="fs-5 fst-italic pt-2 pb-4">
+		<figcaption class="fs-5 fst-italic pt-2 pb-4">
 			<?php echo $image_caption;?>
 		</figcaption>
 		<?php }?>
