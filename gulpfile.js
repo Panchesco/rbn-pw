0532d23275dbbef068842da656745d254cb369d5
lbry-whitmerr <lbry-whitmerr@dhcp-10-142-184-208.uawifi.arizona.edu>
2023-07-19 09:32:35 -0700

    updates link color to increase contrast for language switcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,131 +1,166 @@
 "use strict";
 
-var gulp = require('gulp'),
-  sass = require('gulp-sass')(require('sass')),
-  del = require('del'),
-  uglify = require('gulp-uglify'),
-  cleanCSS = require('gulp-clean-css'),
+var gulp = require("gulp"),
+  sass = require("gulp-sass")(require("sass")),
+  del = require("del"),
+  uglify = require("gulp-uglify"),
+  cleanCSS = require("gulp-clean-css"),
   rename = require("gulp-rename"),
-  merge = require('merge-stream'),
-  htmlreplace = require('gulp-html-replace'),
-  autoprefixer = require('gulp-autoprefixer'),
-  browserSync = require('browser-sync').create(),
-  sourcemaps = require('gulp-sourcemaps');
+  merge = require("merge-stream"),
+  htmlreplace = require("gulp-html-replace"),
+  autoprefixer = require("gulp-autoprefixer"),
+  browserSync = require("browser-sync").create(),
+  sourcemaps = require("gulp-sourcemaps");
 
 // Clean task
-gulp.task('clean', function() {
-  return del(['dist', 'src/css/app.css']);
+gulp.task("clean", function () {
+  return del(["dist", "src/css/app.css"]);
 });
 
 // Copy third party libraries from node_modules into /vendor
-gulp.task('vendor:js', function() {
-  return gulp.src([
-    './node_modules/bootstrap/dist/js/*',
-    './node_modules/@popperjs/core/dist/umd/popper.*'
-  ])
-    .pipe(gulp.dest('./src/js/vendor'));
+gulp.task("vendor:js", function () {
+  return gulp
+    .src([
+      "./node_modules/bootstrap/dist/js/*",
+      "./node_modules/@popperjs/core/dist/umd/popper.*",
+    ])
+    .pipe(gulp.dest("./src/js/vendor"));
 });
 
 // Copy bootstrap-icons from node_modules into /fonts
-gulp.task('vendor:fonts', function() {
-  return  gulp.src([
-    './node_modules/bootstrap-icons/**/*',
-    '!./node_modules/bootstrap-icons/package.json',
-    '!./node_modules/bootstrap-icons/README.md',
-  ])
-    .pipe(gulp.dest('./src/fonts/bootstrap-icons'))
+gulp.task("vendor:fonts", function () {
+  return gulp
+    .src([
+      "./node_modules/bootstrap-icons/**/*",
+      "!./node_modules/bootstrap-icons/package.json",
+      "!./node_modules/bootstrap-icons/README.md",
+    ])
+    .pipe(gulp.dest("./src/fonts/bootstrap-icons"));
 });
 
 // vendor task
-gulp.task('vendor', gulp.parallel('vendor:fonts', 'vendor:js'));
+gulp.task("vendor", gulp.parallel("vendor:fonts", "vendor:js"));
 
 // Copy vendor's js to /dist
-gulp.task('vendor:build', function() {
-  var jsStream = gulp.src([
-    './src/js/vendor/bootstrap.bundle.min.js',
-    './src/js/vendor/popper.min.js'
-  ])
-    .pipe(gulp.dest('./dist/js/vendor'));
-  var fontStream = gulp.src(['./src/fonts/bootstrap-icons/**/*.*']).pipe(gulp.dest('./dist/fonts/bootstrap-icons'));
-  return merge (jsStream, fontStream);
-})
+gulp.task("vendor:build", function () {
+  var jsStream = gulp
+    .src([
+      "./src/js/vendor/bootstrap.bundle.min.js",
+      "./src/js/vendor/popper.min.js",
+    ])
+    .pipe(gulp.dest("./dist/js/vendor"));
+  var fontStream = gulp
+    .src(["./src/fonts/bootstrap-icons/**/*.*"])
+    .pipe(gulp.dest("./dist/fonts/bootstrap-icons"));
+  return merge(jsStream, fontStream);
+});
 
 // Copy Bootstrap SCSS(SASS) from node_modules to /assets/scss/bootstrap
-gulp.task('bootstrap:scss', function() {
-  return gulp.src(['./src/scss/bootstrap/**/*.scss'])
-    .pipe(gulp.dest('./src/scss/bootstrap'));
+gulp.task("bootstrap:scss", function () {
+  return gulp
+    .src(["./src/scss/bootstrap/**/*.scss"])
+    .pipe(gulp.dest("./src/scss/bootstrap"));
 });
 
 // Compile SCSS(SASS) files
-gulp.task('scss', gulp.series('bootstrap:scss', function compileScss() {
-  return gulp.src(['./src/scss/*.scss'])
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./src/css'))
-}));
+gulp.task(
+  "scss",
+  gulp.series("bootstrap:scss", function compileScss() {
+    return gulp
+      .src(["./src/scss/*.scss"])
+      .pipe(
+        sass
+          .sync({
+            outputStyle: "expanded",
+          })
+          .on("error", sass.logError)
+      )
+      .pipe(autoprefixer())
+      .pipe(gulp.dest("./src/css"));
+  })
+);
 
 // Minify CSS
-gulp.task('css:minify', gulp.series('scss', function cssMinify() {
-  return gulp.src("./src/css/*.css")
-    .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(browserSync.stream());
-}));
+gulp.task(
+  "css:minify",
+  gulp.series("scss", function cssMinify() {
+    return gulp
+      .src("./src/css/*.css")
+      .pipe(cleanCSS())
+      .pipe(
+        rename({
+          suffix: ".min",
+        })
+      )
+      .pipe(gulp.dest("./dist/css"))
+      .pipe(browserSync.stream());
+  })
+);
 
 // Minify Js
-gulp.task('js:minify', function () {
-  return gulp.src([
-    './src/js/app.js'
-  ])
+gulp.task("js:minify", function () {
+  return gulp
+    .src(["./src/js/app.js"])
     .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(
+      rename({
+        suffix: ".min",
+      })
+    )
+    .pipe(gulp.dest("./dist/js"))
     .pipe(browserSync.stream());
 });
 
 // Replace HTML block for Js and Css file to min version upon build and copy to /dist
-gulp.task('replaceHtmlBlock', function () {
-  return gulp.src(['*.html'])
-    .pipe(htmlreplace({
-      'js': 'src/js/app.min.js',
-      'css': 'src/css/app.min.css'
-    }))
-    .pipe(gulp.dest('dist/'));
+gulp.task("replaceHtmlBlock", function () {
+  return gulp
+    .src(["*.html"])
+    .pipe(
+      htmlreplace({
+        js: "src/js/app.min.js",
+        css: "src/css/app.min.css",
+      })
+    )
+    .pipe(gulp.dest("dist/"));
 });
 
 // Configure the browserSync task and watch file path for change
-gulp.task('dev', function browserDev(done) {
+gulp.task("dev", function browserDev(done) {
   browserSync.init({
-    proxy: "https://staging.rlw.digitalscholarship.library.arizona.dev",
-	https: true
+    proxy: "https://bordernarrative.lib.arizona.local",
+    https: true,
   });
-  gulp.watch(['src/scss/*.scss','src/scss/**/*.scss','!src/scss/bootstrap/**'], gulp.series('css:minify', function cssBrowserReload (done) {
-    browserSync.reload();
-    done(); //Async callback for completion.
-  }));
-  gulp.watch('src/js/app.js', gulp.series('js:minify', function jsBrowserReload (done) {
-    browserSync.reload();
-    done();
-  }));
-  gulp.watch(['*.html','**/*.php']).on('change', browserSync.reload);
+  gulp.watch(
+    ["src/scss/*.scss", "src/scss/**/*.scss", "!src/scss/bootstrap/**"],
+    gulp.series("css:minify", function cssBrowserReload(done) {
+      browserSync.reload();
+      done(); //Async callback for completion.
+    })
+  );
+  gulp.watch(
+    "src/js/app.js",
+    gulp.series("js:minify", function jsBrowserReload(done) {
+      browserSync.reload();
+      done();
+    })
+  );
+  gulp.watch(["*.html", "**/*.php"]).on("change", browserSync.reload);
   done();
 });
 
 // Build task
-gulp.task("build", gulp.series(gulp.parallel('css:minify', 'js:minify', 'vendor'), 'vendor:build', function copyAssets() {
-  return gulp.src([
-    '*.html',
-    "dist/imgs/**"
-  ], { base: './'})
-    .pipe(gulp.dest('dist'));
-}));
+gulp.task(
+  "build",
+  gulp.series(
+    gulp.parallel("css:minify", "js:minify", "vendor"),
+    "vendor:build",
+    function copyAssets() {
+      return gulp
+        .src(["*.html", "dist/imgs/**"], { base: "./" })
+        .pipe(gulp.dest("dist"));
+    }
+  )
+);
 
 // Default task
-gulp.task("default", gulp.series("clean", 'build', 'replaceHtmlBlock'));
+gulp.task("default", gulp.series("clean", "build", "replaceHtmlBlock"));
--- template-parts/global-header.php
+++ template-parts/global-header.php
@@ -1,8 +1,14 @@
 <header id="global-header" class="has-ivory-buff-background-color">
 	<nav id="header" class="navbar navbar-expand-md container" aria-label="Global Navigation">
-			<div class="navbar-toggler" data-bs-toggle="collapse" aria-controls="navbar" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle global navigation', 'rbn-pw' ); ?>">
+			<div class="navbar-toggler" data-bs-toggle="collapse" aria-controls="navbar" aria-expanded="false" aria-label="<?php esc_attr_e(
+     "Toggle global navigation",
+     "rbn-pw"
+   ); ?>">
 				<div><input type="checkbox" class="visually-hidden" id="hi">
-				  	<label id="toggle-label" class="menu me-auto" for="hi"><span><?php echo _e('Toggle Mobile Navigation','rbn-pw');?></span>
+				  	<label id="toggle-label" class="menu me-auto" for="hi"><span><?php echo _e(
+         "Toggle Mobile Navigation",
+         "rbn-pw"
+       ); ?></span>
 					  <div class="hamburger-wrapper">
 				  		<div class="bar"></div>
 				  		<div class="bar"></div>
@@ -13,56 +19,68 @@
 			</div>
 			<div id="navbar" class="collapse navbar-collapse">
 				<div class="d-flex ms-xl-auto flex-column">
-			<?php
-				// Loading WordPress Custom Menu (theme_location).
-				wp_nav_menu(
-					array(
-						'menu_class'     => 'navbar-nav p-0 m-0 ms-md-auto has-sans-serif-font-family strtoupper order-2',
-						'container'      => '',
-						'fallback_cb'    => 'WP_Bootstrap_Navwalker::fallback',
-						'walker'         => new WP_Bootstrap_Navwalker(),
-						'theme_location' => 'main-menu'
-					)
-				);
-			?>
+			<?php // Loading WordPress Custom Menu (theme_location).
+
+wp_nav_menu([
+     "menu_class" =>
+       "navbar-nav p-0 m-0 ms-md-auto has-sans-serif-font-family strtoupper order-2",
+     "container" => "",
+     "fallback_cb" => "WP_Bootstrap_Navwalker::fallback",
+     "walker" => new WP_Bootstrap_Navwalker(),
+     "theme_location" => "main-menu",
+   ]); ?>
 			<div class="order-1 d-flex justify-content-xl-end">
-				<?php if( is_plugin_active('translatepress-multilingual/index.php') ) :?>
+				<?php if (is_plugin_active("translatepress-multilingual/index.php")): ?>
 				<?php $array = trp_custom_language_switcher(); ?>
-				<?php //IMPORTANT! You need to have data-no-translation on the wrapper with the links or TranslatePress will automatically translate them in a secondary language. ?>
+				<?php
+      //IMPORTANT! You need to have data-no-translation on the wrapper with the links or TranslatePress will automatically translate them in a secondary language.
+      ?>
 				<ul id="language-switcher" data-no-translation class="d-flex py-5 py-lg-2">
-					<?php foreach ($array as $name => $item){ ?>
+					<?php foreach ($array as $name => $item) { ?>
 							<li class="p-0">
-								<?php if( get_locale() != $item['language_code'] ) : ?>
-								<a href="<?php echo $item['current_page_url']?>" tabindex="0"><?php echo trim( $item['language_name'] );?></a>
-								<?php else :?>
-								<?php echo trim( $item['language_name']); ?>
-							<?php endif;?>
+								<?php if (get_locale() != $item["language_code"]): ?>
+								<a href="<?php echo $item["current_page_url"]; ?>" tabindex="1"><?php echo trim(
+  htmlentities($item["language_name"])
+); ?></a>
+								<?php else: ?>
+								<?php echo trim($item["language_name"]); ?>
+							<?php endif; ?>
 							</li>
 					<?php } ?>
 				</ul>
 			</div>
-			<?php endif;?>
+			<?php endif; ?>
 		</div>
 	</div><!-- /.navbar-collapse -->
 	</nav>
 	<div id="butterfly" class="container">
 		<div class="row d-flex justify-content-between">
 			<div class="col-12 col-xl-6 d-flex flex-column">
-				<?php global $post;  if( is_home() || is_front_page() ) : ?>
-				<h1><?php bloginfo('sitename');?></h1>
-				<h2 class="h2"><?php bloginfo('description');?></h2>
-				<?php elseif( isset($post->post_type ) && in_array($post->post_type,['news','events','contributor']) ) : ?>
-				<h1 class="fw-lighter order-2"><?php echo get_post_type_object($post->post_type)->label;?></h1>
-				<h2 class="h1 order-1"><?php bloginfo('sitename');?></h2>
+				<?php
+    global $post;
+    if (is_home() || is_front_page()): ?>
+				<h1><?php bloginfo("sitename"); ?></h1>
+				<h2 class="h2"><?php bloginfo("description"); ?></h2>
+				<?php elseif (
+      isset($post->post_type) &&
+      in_array($post->post_type, ["news", "events", "contributor"])
+    ): ?>
+				<h1 class="fw-lighter order-2"><?php echo get_post_type_object($post->post_type)
+      ->label; ?></h1>
+				<h2 class="h1 order-1"><?php bloginfo("sitename"); ?></h2>
 				<?php else: ?>
-				<?php if( isset( $post->post_type ) ) :?>
+				<?php if (isset($post->post_type)): ?>
 				<h1 class="fw-lighter order-2"><?php echo $post->post_title; ?></h1>
-				<?php endif ;?>
-				<h2 class="h1 order-1"><?php bloginfo('sitename');?></h2>
-				<?php endif;?>
+				<?php endif; ?>
+				<h2 class="h1 order-1"><?php bloginfo("sitename"); ?></h2>
+				<?php endif;
+    ?>
 			</div>
 			<div class="d-none d-xl-block col-xl-6">
-			<?php get_template_part('template-parts/header-spotlight','header-spotlight');?>
+			<?php get_template_part(
+     "template-parts/header-spotlight",
+     "header-spotlight"
+   ); ?>
 			</div>
 		</div><!-- end .row -->
 	</div><!-- end .container -->
